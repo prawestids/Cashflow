194254dd69a3077f2d7b5e46260a4dd6880fc3b2
prawestids <prawestidwisalindri7@gmail.com>
2020-03-19 19:06:51 +0700

    cashflow

--- app/src/main/java/com/dhanifudin/cashflow/SaveActivity.java
+++ app/src/main/java/com/dhanifudin/cashflow/SaveActivity.java
@@ -1,11 +1,13 @@
 package com.dhanifudin.cashflow;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.dhanifudin.cashflow.models.Transaction;
 
@@ -26,8 +28,48 @@ public class SaveActivity extends AppCompatActivity {
         amountInput = findViewById(R.id.input_amount);
         typeRadioGroup = findViewById(R.id.group_type);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            item = extras.getParcelable(MainActivity.TRANSACTION_KEY);
+            index = extras.getInt(MainActivity.INDEX_KEY, 0);
+            descriptionInput.setText(item.getDescription());
+            amountInput.setText(String.valueOf(item.getAmount()));
+            if (item.getType() == Transaction.Type.DEBIT) {
+                typeRadioGroup.check(R.id.radio_debit);
+            } else if (item.getType() == Transaction.Type.CREDIT) {
+                typeRadioGroup.check(R.id.radio_credit);
+            }
+        }
+    }
+    private Transaction.Type getCheckedType() {
+        if (typeRadioGroup.getCheckedRadioButtonId() == R.id.radio_debit) {
+            return Transaction.Type.DEBIT;
+        } else if (typeRadioGroup.getCheckedRadioButtonId() == R.id.radio_credit) {
+            return Transaction.Type.CREDIT;
+        }
+        return Transaction.Type.EMPTY;
     }
 
     public void handleSubmit(View view) {
+        String description = descriptionInput.getText().toString();
+        int amount = Integer.parseInt(amountInput.getText().toString());
+        Transaction.Type type = getCheckedType();
+
+        item.setDescription(description);
+        item.setAmount(amount);
+        item.setType(type);
+
+        if(description.equals("")|| (amount) ==0 ){
+            Toast.makeText(this, "data tidak boleh kososng", Toast.LENGTH_SHORT).show();
+        } else if(typeRadioGroup.getCheckedRadioButtonId()==-1){
+            Toast.makeText(this, "data tidak boleh kososng", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent intent = new Intent();
+            intent.putExtra(MainActivity.TRANSACTION_KEY, item);
+            intent.putExtra(MainActivity.INDEX_KEY, index);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }
